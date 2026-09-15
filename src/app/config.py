b081480f105bb0136b75c6bda87a5924cb43876a
Jatin Choudhary <jatinchoudhary1712@gmail.com>
2026-09-15 17:11:21 +0000

    Base Setting Added

---
+++ src/app/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    app_name :str="RepoPilot"
+    environment: str = "development"
+    
+    model_config = SettingsConfigDict(
+        env_file = ".env",
+        env_file_encoding='utf-8',
+        extra="ignore"
+    )
+    
+settings = Settings()
